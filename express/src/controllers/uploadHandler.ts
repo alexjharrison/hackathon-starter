8518b0b96fed2b88ccd3af18a8531941e73260f3
Alex Harrison <alexjharrison@gmail.com>
2021-09-25 23:02:49 -0400

    create updateHandler

--- express/src/controllers/uploadHandler.ts
+++ express/src/controllers/uploadHandler.ts
@@ -2,7 +2,11 @@ import { Router } from "express";
 
 const router = Router();
 
+// return file by uuid key
 router.get("/:key", (req, res) => {});
 
+// save file to /static folder
+// save metadata to db
 router.post("/", (req, res) => {});
+
 export const uploadHandler = router;
